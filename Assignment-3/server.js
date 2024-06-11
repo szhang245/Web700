842830227111ed1abbdf2fda48ba4d1b19ebdc91
szhang245 <szhang245@myseneca.ca>
2024-06-11 02:16:07 -0400

    new file:   Assignment-3/.DS_Store 	new file:   Assignment-3/data/courses.json 	new file:   Assignment-3/data/students.json 	new file:   Assignment-3/modules/.DS_Store 	new file:   Assignment-3/modules/collegeData.js 	new file:   Assignment-3/package-lock.json 	new file:   Assignment-3/package.json 	new file:   Assignment-3/server.js 	new file:   Assignment-3/views/about.html 	new file:   Assignment-3/views/home.html 	new file:   Assignment-3/views/htmlDemo.html

---
+++ Assignment-3/server.js
@@ -0,0 +1,191 @@
+/*********************************************************************************
+*  WEB700 – Assignment 3
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+*
+*  Name: _Shuai Zhang_ Student ID: _136898236_ Date: _June 10, 2024_
+*
+********************************************************************************/
+var HTTP_PORT = process.env.HTTP_PORT || 8080;
+var express = require("express");
+var path = require("path");
+var app = express();
+var collegeData = require("./modules/collegeData");
+
+app.set('json spaces', 2)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+})
+
+app.get("/htmlDemo", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
+})
+
+app.get("/students", (req, res) => {
+    const course = parseInt(req.query.course);
+    collegeData.getAllStudents()
+    .then( (students) => {
+        if(course) {
+            collegeData.getStudentsByCourse(course)
+            .then( (result) => {
+                res.json(result);
+            }
+            )
+            .catch( () => {
+                res.json({
+                    message: "no course results"
+                })
+            }
+            )
+        }
+        else {
+            res.json(students);
+        }
+    }
+    )
+    .catch( () => {
+        res.json({
+            message: "no results"
+        })
+    })
+})
+
+app.get('/student/:num', (req, res) => {
+    const num = parseInt(req.params.num);
+    collegeData.getStudentByNum(num)
+    .then(
+        (student) => {
+            res.json(student);
+        }
+    )
+    .catch( () => {
+        res.json(
+            {message: "no student results"}
+        )
+    }
+    )
+}
+)
+
+app.get('/tas', (req, res) => {
+    collegeData.getTAs()
+    .then( (tas) => {
+        res.json(tas);
+    }
+    )
+    .catch( () => {
+        res.json(
+            {message: "no tas results"}
+        )
+    })
+}
+)
+
+app.get('/courses', (req, res) => {
+    collegeData.getCourses()
+    .then( (courses) => {
+        res.json(courses);
+    }
+    )
+    .catch( () => {
+        res.json(
+            {message: "no courses results"}
+        )
+    })
+}
+)
+
+app.all(
+    '*', (req, res) => {
+        res.status(404).send("Page Not THERE, Are you sure of the path?");
+    }
+)
+
+collegeData.initialize()
+.then( () => {
+    app.listen(HTTP_PORT, () => {console.log("server listening on port: " + HTTP_PORT)});
+}
+)
+.catch( err => {
+    console.log*(err);
+})
+
+
+
+
+
+// const collegeData = require("./modules/collegeData");
+// // collegeData.getStudentsByCourse(2)
+// // .then(
+// //     res => {
+// //         console.log(res);
+// //     }
+// // )
+// // .catch(err => {
+// //     console.log(err);
+// // }
+// // )
+
+// collegeData.initialize()
+// .then( () => {
+//    collegeData.getAllStudents()
+//    .then( () => {
+//         collegeData.getStudentsByCourse(1)
+//         .then(
+//             (res) => {
+//                 console.log(res);
+//             }
+//         )
+//         .catch( err => {
+//             console.log(err);
+//         }
+//         )
+//    }
+//    )
+//    .then( () => {
+//         collegeData.getStudentByNum(1000)
+//         .then(
+//             (res) => {
+//                 console.log(res);
+//             }
+//         )
+//         .catch( err => {
+//             console.log(err)
+//         })
+//    }
+
+//    )
+//    .catch( err => {
+//         console.log(err);
+//    })
+// }
+// )
+// .then( () => {
+//     collegeData.getCourses()
+//     .then ( (courses) => {
+//         console.log(`Successfully retrieved ${courses.length} courses`)
+//     }
+//     )
+//     .catch( err => {
+//         console.log(err);
+//     })
+// }
+// )
+// .then( () => {
+//     collegeData.getTAs()
+//     .then ( (TAs) => {
+//         console.log(`Successfully retrieved ${TAs.length} TAs`)
+//     })
+//     .catch ( err => {
+//         console.log(err);
+//     })
+// })
+// .catch( err => {
+//     console.log(err);
+// })
